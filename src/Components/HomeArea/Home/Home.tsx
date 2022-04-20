12bd8bc2b62a8adda130b4865e5de26300227327
erelzohar <zoharerel@gmail.com>
2022-04-20 19:08:19 +0300

    basic

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -5,6 +5,13 @@ import "./Home.css";
 import image1 from '../../../assets/images/1.jpg'
 import image2 from '../../../assets/images/2.jpg'
 import image3 from '../../../assets/images/3.jpg'
+import products1 from '../../../assets/images/products-1.jpg'
+import products2 from '../../../assets/images/products-2.jpg'
+import products3 from '../../../assets/images/products-3.jpg'
+import slide1 from '../../../assets/images/slide-1.jpg'
+import slide2 from '../../../assets/images/slide-2.jpg'
+import slide3 from '../../../assets/images/slide-3.jpg'
+import slide4 from '../../../assets/images/slide-4.jpg'
 
 function Home(): JSX.Element {
 
@@ -29,7 +36,10 @@ function Home(): JSX.Element {
             <div dir="rtl" id="slide">
                 <h1 ref={slideRef}>החלקות</h1>
                 <img src={image3} alt="" />
-                <img src={image3} alt="" />
+                <img src={slide1} alt="" />
+                <img src={slide2} alt="" />
+                <img src={slide3} alt="" />
+                <img src={slide4} alt="" />
             </div>
 
             <div dir="rtl" id="add">
@@ -50,6 +60,9 @@ function Home(): JSX.Element {
             <div dir="rtl" id="products">
                 <h1 ref={productsRef} >מוצרים וקורסים</h1>
                 <img src={image2} alt="" />
+                <img src={products1} alt="" />
+                <img src={products2} alt="" />
+                <img src={products3} alt="" />
             </div>
 
             <SpeedDialComponent />
